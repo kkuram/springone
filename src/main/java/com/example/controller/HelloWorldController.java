a91c033421140fb684225f8af7de15ae25e83db1
Kalyan Kuram <kalyan.kuram@live.com>
2016-08-03 17:15:10 -0700

    remove index url

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -13,10 +13,6 @@ public class HelloWorldController {
 		return ResponseEntity.ok("I am at SpringOne conference and i deployed to Heroku");
 	}
 
-@GetMapping("/")
-	public ResponseEntity<String> index(){
-		return ResponseEntity.ok("I am at SpringOne conference and i deployed to Heroku called from Index");
-	}
 
 
 }
